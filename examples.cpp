c72de0adc082954014dd76e052cfc59705ae2cee
eeggers <eeggers@cord.edu>
2013-07-19 22:18:34 -0500

    Update examples.cpp

--- examples.cpp
+++ examples.cpp
@@ -5,9 +5,9 @@
 
 main(){
 
-	//instantiate a new PCF8574 device.
+	//instantiate a new PCF8574 object.
 	//on my machine, the i2c bus I want to talk to can be accessed via /dev/i2c-1.
-	//the pcf8574 chip I want to talk to is at address 32 (0x20)
+	//the pcf8574 chip I want to talk to is at address 32 (0x20).
 	//this is merely an example.
 	//be exceptionally careful when experimenting. see license
 	PCF8574 * device;
